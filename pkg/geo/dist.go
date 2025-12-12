ff826427c2a98dfaa8e8edebfa9a72a3996f5fa2
shamhi <shomakakdela@gmail.com>
2025-12-12 17:01:48 +0300

    feat: add calculating haversine distance method

---
+++ pkg/geo/dist.go
@@ -0,0 +1,19 @@
+package geo
+
+import "math"
+
+func HaversineDistance(lat1, lon1, lat2, lon2 float64) float64 {
+	const R = 6371e3
+
+	phi1 := lat1 * math.Pi / 180
+	phi2 := lat2 * math.Pi / 180
+	deltaPhi := (lat2 - lat1) * math.Pi / 180
+	deltaLambda := (lon2 - lon1) * math.Pi / 180
+
+	a := math.Sin(deltaPhi/2)*math.Sin(deltaPhi/2) +
+		math.Cos(phi1)*math.Cos(phi2)*
+			math.Sin(deltaLambda/2)*math.Sin(deltaLambda/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	return R * c
+}
